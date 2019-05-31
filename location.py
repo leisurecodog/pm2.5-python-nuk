e08a8838fab14dd8ba13e51ab483469e60d7b67a
xiefengan <30953779+xiefengan@users.noreply.github.com>
2019-05-31 23:29:23 +0800

    update

--- location.py
+++ location.py
@@ -8,14 +8,12 @@ def get_pos():
     # use ip to get latitude and longitude
     url = 'https://get.geojs.io/v1/ip/geo/' + my_ip + '.json'
     geo_request = requests.get(url)
-    print(geo_request.json())
-    geo_latitude = geo_request.json()['latitude']
-    geo_longitude = geo_request.json()['longitude']
-    return geo_latitude,geo_longitude
+    dic = geo_request.json()
+    return dic
 
 
 
 if __name__ == '__main__':
-    a, b = get_pos()
-    print(a,b,sep=', ')
+    dic = get_pos()
+    print(dic['city'])
 
--- pm2dot5.py
+++ pm2dot5.py
@@ -28,37 +28,59 @@ def catch():
         #AQI1    
         soup_AQI1 = soup.find_all(id=re.compile("^ctl05_gv_ctl(\d\d)_labPSI"))
         for s, i in zip(soup_AQI1, station.keys()):
+            if s.text == '':
+                station[i].append('-1')
+                continue
+            
             station[i].append(s.text)
 
         #ppb
         soup_ppb = soup.find_all(id=re.compile("^ctl05_gv_ctl(\d\d)_labO3"))
         for s, i in zip(soup_ppb, station.keys()):
+            if s.text == '':
+                station[i].append('-1')
+                continue
             station[i].append(s.text)
 
         #PM25
         soup_PM25 = soup.find_all(id=re.compile("^ctl05_gv_ctl(\d\d)_labPM25"))
         for s, i in zip(soup_PM25, station.keys()):
+            if s.text == '':
+                station[i].append('-1')
+                continue
             station[i].append(s.text)
 
         #PM10
         soup_PM10 = soup.find_all(id=re.compile("^ctl05_gv_ctl(\d\d)_labPM10"))
         for s, i in zip(soup_PM10, station.keys()):
+            if s.text == '':
+                station[i].append('-1')
+                continue
             station[i].append(s.text)
 
         #CO
         soup_CO = soup.find_all(id=re.compile("^ctl05_gv_ctl(\d\d)_labCO"))
         for s, i in zip(soup_CO, station.keys()):
+            if s.text == '':
+                station[i].append('-1')
+                continue
             station[i].append(s.text)
 
         #SO2
         soup_SO2 = soup.find_all(id=re.compile("^ctl05_gv_ctl(\d\d)_labSO2"))
         for s, i in zip(soup_SO2, station.keys()):
+            if s.text == '':
+                station[i].append('-1')
+                continue
             station[i].append(s.text)
 
         #NO2
         soup_NO2 = soup.find_
         soup_NO2 = soup.find_all(id=re.compile("^ctl05_gv_ctl(\d\d)_labNO2"))
         for s, i in zip(soup_NO2, station.keys()):
+            if s.text == '':
+                station[i].append('-1')
+                continue
             station[i].append(s.text)
 
 
@@ -73,7 +95,7 @@ def main_fun():
 
     catch()
 
-    
+    """
     now = datetime.datetime.now()
     year = now.year
     month = now.month
@@ -87,18 +109,19 @@ def main_fun():
     else:
         sched_time = datetime.datetime(year, month, day, hour + 1, 3, 0)
 
-    print(now)
+    
     while True:
         now = datetime.datetime.now()
         if sched_time < now < sched_time + datetime.timedelta(hours = 1) and sched_time + datetime.timedelta(hours = 1) - now < 1:
             sched_time = sched_time + datetime.timedelta(hours = 1)
             catch()
     
-if __name__ == '__main__':
-    main_fun()
-    catch()
+    """
+main_fun()
 
-    print("\n\n\n\n\n\n爬蟲程式 End")
+
+print("\n\n\n\n\n\n爬蟲程式 End")
+
 
 
 
